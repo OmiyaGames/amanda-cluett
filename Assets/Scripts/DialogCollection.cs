9142166aee89679654629b797e0a8081fa26478f
Taro Omiya <taro.omiya@omiyagames.com>
2015-09-19 15:53:30 -0400

    Starting on a dialog system.  Have the GUI done, now just need the functionality.

---
+++ Assets/Scripts/DialogCollection.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.Collections;
+
+public class DialogCollection : MonoBehaviour, IList<Dialog>
+{
+    readonly List<Dialog> allDialogs = new List<Dialog>();
+
+    #region Overrides
+    public Dialog this[int index]
+    {
+        get
+        {
+            return allDialogs[index];
+        }
+        set
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public int Count
+    {
+        get
+        {
+            return allDialogs.Count;
+        }
+    }
+
+    public bool IsReadOnly
+    {
+        get
+        {
+            return true;
+        }
+    }
+
+    public void Add(Dialog item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(Dialog item)
+    {
+        return allDialogs.Contains(item);
+    }
+
+    public void CopyTo(Dialog[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<Dialog> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int IndexOf(Dialog item)
+    {
+        return allDialogs.IndexOf(item);
+    }
+
+    public void Insert(int index, Dialog item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(Dialog item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveAt(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return allDialogs.GetEnumerator();
+    }
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        allDialogs.Clear();
+        Dialog childDialog = null;
+	    for(int i = 0; i < transform.childCount; ++i)
+        {
+            childDialog = transform.GetChild(i).GetComponent<Dialog>();
+            if(childDialog != null)
+            {
+                allDialogs.Add(childDialog);
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/DialogPanel.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class DialogPanel : MonoBehaviour
+{
+    public enum Character
+    {
+        Husband,
+        Wife
+    }
+
+    [System.Serializable]
+    public struct CharacterColorPair
+    {
+        public Character character;
+        public Color textColor;
+    }
+
+    [Header("Components")]
+    [SerializeField]
+    Animator animator;
+    [SerializeField]
+    Text presentText;
+    [SerializeField]
+    Text nextText;
+
+    [Header("Animation Fields")]
+    [SerializeField]
+    string visibleBoolField = "visible";
+    [SerializeField]
+    string nextTrigger = "next";
+
+    [Header("Dialogs")]
+    [SerializeField]
+    CharacterColorPair[] characterMap;
+    [SerializeField]
+    DialogCollection initialDialogs;
+
+    public readonly Dictionary<Character, Color> characterTextColorMap = new Dictionary<Character, Color>();
+
+    DialogCollection currentDialogs = null;
+    int dialogIndex = -1;
+
+    public DialogCollection CurrentDialogCollection
+    {
+        get
+        {
+            return currentDialogs;
+        }
+    }
+
+    public Dialog CurrentDialog
+    {
+        get
+        {
+            Dialog returnDialog = null;
+            if((CurrentDialogCollection != null) && (dialogIndex >= 0) && (dialogIndex < CurrentDialogCollection.Count))
+            {
+                returnDialog = CurrentDialogCollection[dialogIndex];
+            }
+            return returnDialog;
+        }
+    }
+
+    void Start()
+    {
+        // Setup character map
+        characterTextColorMap.Clear();
+        for(int i = 0; i < characterMap.Length; ++i)
+        {
+            characterTextColorMap.Add(characterMap[i].character, characterMap[i].textColor);
+        }
+    }
+
+    public void OnNextClicked()
+    {
+        // FIXME: go to next dialog
+
+        // For not, just switch texts
+        animator.SetTrigger(nextTrigger);
+    }
+
+    public void SwapText()
+    {
+        presentText.text = nextText.text;
+        presentText.color = nextText.color;
+    }
+}
