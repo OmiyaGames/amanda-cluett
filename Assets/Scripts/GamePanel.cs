8d953b82eccff558196f7fb2b4f9f29ecac77938
Taro Omiya <taro.omiya@omiyagames.com>
2015-09-19 22:52:49 -0400

    Adding GamePowerUp to all the buttons.  They'll be responsible for updating the button.  OnClick event hasn't been implemented yet, though.

--- Assets/Scripts/GamePanel.cs
+++ Assets/Scripts/GamePanel.cs
@@ -7,8 +7,25 @@ public class GamePanel : MonoBehaviour
     public const string CurrencyKey = "CurrentCurrency";
     public const string VictoryKey = "VictoryPoints";
     public const string IncomeKey = "ReceivingIncome";
+    public const string DressesKey = "Dresses";
+    public const string BooksKey = "Books";
+    public const string SewingMachineKey = "SewingMachines";
+    public const string FurnituresKey = "Furnitures";
 
-    public event System.Action<GamePanel> OnMoneyGained;
+    public enum Units
+    {
+        None = -1,
+        Cents = 0,
+        Books,
+        Dresses,
+        SewingMachines,
+        Furnitures,
+        HusbandsIncome,
+        VictoryPoints
+    }
+
+    public event System.Action<GamePanel> OnCurrencyChanged;
+    public event System.Action<GamePanel> OnSupplyChanged;
 
     [Header("Labels")]
     [SerializeField]
@@ -20,12 +37,21 @@ public class GamePanel : MonoBehaviour
     [SerializeField]
     GameObject victorySet;
 
-    readonly StringBuilder builder = new StringBuilder();
-    float lastStarted = -1f;
+    // Important stuff
     int currencyCents = 0;
     int victoryPoints = 0;
+
+    // Resources
+    int husbandsIncome = 100;
+    int dresses = 0;
+    int books = 0;
+    int sewingMachines = 0;
+    int furnitures = 0;
+
+    // Temporary stuff
+    readonly StringBuilder builder = new StringBuilder();
+    float lastStarted = -1f;
     int tempDisplay = 0;
-    bool isReceivingIncome = true;
 
     #region Properties
     public int CurrentCurrencyCents
@@ -40,7 +66,7 @@ public class GamePanel : MonoBehaviour
             {
                 currencyCents = value;
                 PlayerPrefs.SetInt(CurrencyKey, currencyCents);
-                Start();
+                OnMoneyChanged();
             }
         }
     }
@@ -64,18 +90,98 @@ public class GamePanel : MonoBehaviour
         }
     }
 
-    public bool IsReceivingIncome
+    public int HusbandsIncome
     {
         get
         {
-            return isReceivingIncome;
+            return husbandsIncome;
         }
         set
         {
-            if (isReceivingIncome != value)
+            if (husbandsIncome != value)
             {
-                isReceivingIncome = value;
-                PlayerPrefs.SetInt(IncomeKey, (isReceivingIncome ? 1 : 0));
+                husbandsIncome = value;
+                PlayerPrefs.SetInt(IncomeKey, husbandsIncome);
+            }
+        }
+    }
+
+    public int CurrentDresses
+    {
+        get
+        {
+            return dresses;
+        }
+        set
+        {
+            if ((value >= 0) && (dresses != value))
+            {
+                dresses = value;
+                PlayerPrefs.SetInt(DressesKey, dresses);
+                if(OnSupplyChanged != null)
+                {
+                    OnSupplyChanged(this);
+                }
+            }
+        }
+    }
+
+    public int CurrentBooks
+    {
+        get
+        {
+            return books;
+        }
+        set
+        {
+            if ((value >= 0) && (books != value))
+            {
+                books = value;
+                PlayerPrefs.SetInt(BooksKey, books);
+                if (OnSupplyChanged != null)
+                {
+                    OnSupplyChanged(this);
+                }
+            }
+        }
+    }
+
+    public int CurrentSewingMachines
+    {
+        get
+        {
+            return sewingMachines;
+        }
+        set
+        {
+            if ((value >= 0) && (sewingMachines != value))
+            {
+                sewingMachines = value;
+                PlayerPrefs.SetInt(SewingMachineKey, sewingMachines);
+                if (OnSupplyChanged != null)
+                {
+                    OnSupplyChanged(this);
+                }
+            }
+        }
+    }
+
+    public int CurrentFurnitures
+    {
+        get
+        {
+            return furnitures;
+        }
+        set
+        {
+            if ((value >= 0) && (furnitures != value))
+            {
+                furnitures = value;
+                PlayerPrefs.SetInt(FurnituresKey, furnitures);
+                if (OnSupplyChanged != null)
+                {
+                    OnSupplyChanged(this);
+                }
             }
         }
     }
@@ -95,27 +201,47 @@ public class GamePanel : MonoBehaviour
         }
     }
 
+    public string CentsToString(int cents)
+    {
+        // Setup the currency text
+        builder.Length = 0;
+        builder.Append('$');
+
+        // Get the dollar amount
+        tempDisplay = (cents / 100);
+        builder.Append(tempDisplay.ToString("N0"));
+
+        // Get the cents
+        tempDisplay = (cents % 100);
+        builder.Append('.');
+        builder.Append(tempDisplay.ToString("00"));
+        return builder.ToString();
+    }
+
+    #region Events
     // Use this for initialization
     void Awake ()
     {
         // Retrieve settings
         currencyCents = PlayerPrefs.GetInt(CurrencyKey, 0);
         victoryPoints = PlayerPrefs.GetInt(VictoryKey, 0);
-        isReceivingIncome = (PlayerPrefs.GetInt(IncomeKey, 1) != 0);
+
+        husbandsIncome = PlayerPrefs.GetInt(IncomeKey, 100);
+        dresses = PlayerPrefs.GetInt(DressesKey, 0);
+        books = PlayerPrefs.GetInt(BooksKey, 0);
+        sewingMachines = PlayerPrefs.GetInt(SewingMachineKey, 0);
+        furnitures = PlayerPrefs.GetInt(FurnituresKey, 0);
     }
-	
+
     void Start()
     {
-        // Update labels
-        UpdateLabels();
-
-        // Check the amount of money gained
-        if (OnMoneyGained != null)
+        OnMoneyChanged();
+        if(OnSupplyChanged != null)
         {
-            OnMoneyGained(this);
+            OnSupplyChanged(this);
         }
     }
-
+	
     // Update is called once per frame
     void Update ()
     {
@@ -123,36 +249,21 @@ public class GamePanel : MonoBehaviour
 	    if((lastStarted > 0) && ((Time.time - lastStarted) > 1f))
         {
             // Increment currency
-            currencyCents += CurrentVictoryPoints;
-            if (IsReceivingIncome == true)
-            {
-                currencyCents += 100;
-            }
-            PlayerPrefs.SetInt(CurrencyKey, currencyCents);
-            Start();
+            CurrentCurrencyCents += CurrentVictoryPoints + husbandsIncome;
 
             // Save settings
             PlayerPrefs.Save();
+
+            // Reset time
+            lastStarted = Time.time;
         }
     }
+    #endregion
 
     void UpdateLabels()
     {
-        // Setup the currency text
-        builder.Length = 0;
-        builder.Append('$');
-
-        // Get the dollar amount
-        tempDisplay = (CurrentCurrencyCents / 100);
-        builder.Append(tempDisplay.ToString("N0"));
-
-        // Get the cents
-        tempDisplay = (CurrentCurrencyCents % 100);
-        builder.Append('.');
-        builder.Append(tempDisplay.ToString("00"));
-
         // Display the currency
-        currencyLabel.text = builder.ToString();
+        currencyLabel.text = CentsToString(CurrentCurrencyCents);
 
         // Update Victory Points
         if (victoryPoints > 0)
@@ -163,6 +274,18 @@ public class GamePanel : MonoBehaviour
         else
         {
             victorySet.SetActive(false);
+        }
+    }
+
+    void OnMoneyChanged()
+    {
+        // Update labels
+        UpdateLabels();
+
+        // Check the amount of money gained
+        if (OnCurrencyChanged != null)
+        {
+            OnCurrencyChanged(this);
         }
     }
 }
--- Assets/Scripts/GamePowerUp.cs
+++ Assets/Scripts/GamePowerUp.cs
@@ -4,42 +4,113 @@ using System.Collections.Generic;
 
 public class GamePowerUp : MonoBehaviour
 {
-    public enum Units
+    [System.Serializable]
+    public struct Change
     {
-        Cents,
-        Books,
-        Wear
+        public GamePanel.Units units;
+        public int quantity;
     }
 
-    [Header("Components")]
     [SerializeField]
     GamePanel parentPanel = null;
     [SerializeField]
-    Button productButton = null;
+    Button powerUpButton = null;
     [SerializeField]
-    Text productName = null;
+    Change cost;
     [SerializeField]
-    Text priceText = null;
+    Change gain;
 
-    [Header("Save Information")]
-    [SerializeField]
-    string uniqueName = "test";
-    [SerializeField]
-    int victoryPointGained = 0;
-    [SerializeField]
-    int costInCents = 0;
+    Text powerUpLabel = null;
+    string originalProductString = null;
+    int currentQuantity = -1;
 
-    int quantity = 0;
+    System.Action<GamePanel> unitsChanged = null;
 
     // Use this for initialization
     void Awake ()
     {
-        parentPanel.OnMoneyGained += OnMoneyChange;
-	}
-	
-	// Update is called once per frame
-	void OnMoneyChange (GamePanel panel)
+        // Setup private variables
+        powerUpLabel = powerUpButton.GetComponentInChildren<Text>();
+        originalProductString = powerUpLabel.text;
+
+        // Setup text
+        string costText = cost.quantity.ToString();
+        if (cost.units == GamePanel.Units.Cents)
+        {
+            costText = parentPanel.CentsToString(cost.quantity);
+        }
+        powerUpLabel.text = string.Format(originalProductString, costText);
+
+        // Setup events
+        OnDestroy();
+        switch (cost.units)
+        {
+            case GamePanel.Units.Cents:
+                unitsChanged = new System.Action<GamePanel>(OnUnitsChange);
+                parentPanel.OnCurrencyChanged += unitsChanged;
+                break;
+            case GamePanel.Units.Books:
+            case GamePanel.Units.Dresses:
+            case GamePanel.Units.Furnitures:
+            case GamePanel.Units.SewingMachines:
+                unitsChanged = new System.Action<GamePanel>(OnUnitsChange);
+                parentPanel.OnSupplyChanged += unitsChanged;
+                break;
+        }
+    }
+
+    void OnDestroy()
     {
-        productButton.interactable = (panel.CurrentCurrencyCents >= costInCents);
+        switch (cost.units)
+        {
+            case GamePanel.Units.Cents:
+                if (unitsChanged != null)
+                {
+                    parentPanel.OnCurrencyChanged -= unitsChanged;
+                    unitsChanged = null;
+                }
+                break;
+            case GamePanel.Units.Books:
+            case GamePanel.Units.Dresses:
+            case GamePanel.Units.Furnitures:
+            case GamePanel.Units.SewingMachines:
+                if (unitsChanged != null)
+                {
+                    parentPanel.OnSupplyChanged -= unitsChanged;
+                    unitsChanged = null;
+                }
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void OnUnitsChange (GamePanel panel)
+    {
+        // Check for the quantity
+        currentQuantity = -1;
+        switch (cost.units)
+        {
+            case GamePanel.Units.Cents:
+                currentQuantity = panel.CurrentCurrencyCents;
+                break;
+            case GamePanel.Units.Books:
+                currentQuantity = panel.CurrentBooks;
+                break;
+            case GamePanel.Units.Dresses:
+                currentQuantity = panel.CurrentDresses;
+                break;
+            case GamePanel.Units.Furnitures:
+                currentQuantity = panel.CurrentFurnitures;
+                break;
+            case GamePanel.Units.SewingMachines:
+                currentQuantity = panel.CurrentSewingMachines;
+                break;
+        }
+
+        // See if we found the correct quantity
+        if(currentQuantity > 0)
+        {
+            powerUpButton.interactable = (currentQuantity >= cost.quantity);
+        }
     }
 }
